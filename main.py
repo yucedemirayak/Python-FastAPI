07f433f1efc0d9fa57df03cff542dba921fcfc50
yucedemirayak <yucedemirayak@outlook.com>
2022-10-09 19:18:42 +0300

    basic get and post operations with validations

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
 @app.get("/")
 async def root():
-  return {"message": "Hello World"}
+    return {"message": "Welcome to my FastAPI"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "this is your post"}
+
+
+@app.post("/createposts")
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": post}
